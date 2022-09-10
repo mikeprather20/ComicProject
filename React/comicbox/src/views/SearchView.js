08f05c48dd1d824bd2e763bffb24b994ba7812c9
Michael Prather <mikeprather20@gmail.com>
2022-09-10 09:02:18 -0500

    search view

--- React/comicbox/src/views/SearchView.js
+++ React/comicbox/src/views/SearchView.js
@@ -1,10 +1,123 @@
-import React from 'react'
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import CssBaseline from '@mui/material/CssBaseline';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
-export default function SearchView() {
+const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9]; //need to make it contenual or load per scroll?
+
+const theme = createTheme();
+
+export default function ComicBox() {
   return (
-    <div>
-      
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      {/* this is where the navbar goes*/}
+      <main>
+        <Box
+          sx={{
+            bgcolor: 'background.paper',
+            pt: 8,
+            pb: 6,
+          }}
+        >
+          <Container maxWidth="sm">
+            <Typography
+              component="h1"
+              variant="h2"
+              align="center"
+              color="text.primary"
+              gutterBottom
+            >
+              Search for Comic
+            </Typography>
+            {/* in here we put in the search bar */}
+          </Container>
+        </Box>
+        <Container sx={{ py: 8 }} maxWidth="md">
 
-    </div>
-  )
+          <Grid container spacing={4}>
+            {cards.map((card) => (
+              <Grid item key={card} xs={12} sm={6} md={4}>
+                <Card
+                  sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
+                >
+                  <CardMedia
+                    component="img"
+                    sx={{
+                      // 16:9
+                      pt: '56.25%',
+                    }}
+                    image=" IMAGE URL OF THE COMIC HERE "
+                    alt="random"
+                  />
+                  <CardContent sx={{ flexGrow: 1 }}>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      Name of comic
+                    </Typography>
+                    <Typography>
+                      This is where the...
+                      issue number
+                      and volume go.
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button size="small">Add to Box</Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
+      </main>
+    </ThemeProvider>
+  );
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
